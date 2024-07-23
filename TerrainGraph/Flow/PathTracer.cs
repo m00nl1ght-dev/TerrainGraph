abd5606a65bc5977e208947ea4d780de8b3f6e19
m00nl1ght <github@m00nl1ght.dev>
2024-07-23 20:14:28 +0200

    Correct angle of last frame for segments with target

--- TerrainGraph/Flow/PathTracer.cs
+++ TerrainGraph/Flow/PathTracer.cs
@@ -405,7 +405,13 @@ public class PathTracer
                     var chord = Vector2d.Distance(a.pos, node.Position);
 
                     distDelta = angleDelta.Abs() < RadialThreshold ? chord : rad * 0.5 * chord / Math.Sin(0.5 * rad);
-                    distDelta = Math.Min(distDelta, length + task.marginHead - a.dist);
+
+                    var distDeltaMax = length + task.marginHead - a.dist;
+                    if (distDelta > distDeltaMax && distDeltaMax > 0)
+                    {
+                        angleDelta *= distDeltaMax / distDelta;
+                        distDelta = distDeltaMax;
+                    }
 
                     if (_frameBuffer.Count + 1 >= pathNodes.Count)
                     {
