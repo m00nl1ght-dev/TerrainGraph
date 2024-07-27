f70ba2854eb7f9366d59755a20680f779bbf9534
m00nl1ght <github@m00nl1ght.dev>
2024-07-27 19:49:02 +0200

    Add thin limit to NodeGridMorphGroupFilter

--- TerrainGraph/Nodes/Grid/NodeGridMorphGroupFilter.cs
+++ TerrainGraph/Nodes/Grid/NodeGridMorphGroupFilter.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using NodeEditorFramework;
+using TerrainGraph.Flow;
 using TerrainGraph.Util;
 using UnityEngine;
 
@@ -27,12 +28,16 @@ public class NodeGridMorphGroupFilter : NodeBase
     [ValueConnectionKnob("Max size", Direction.In, ValueFunctionConnection.Id)]
     public ValueConnectionKnob MaxGroupSizeKnob;
 
+    [ValueConnectionKnob("Thin limit", Direction.In, ValueFunctionConnection.Id)]
+    public ValueConnectionKnob ThinLimitKnob;
+
     [ValueConnectionKnob("Output", Direction.Out, GridFunctionConnection.Id)]
     public ValueConnectionKnob OutputKnob;
 
     public double Threshold = 1;
     public double MinGroupSize;
     public double MaxGroupSize;
+    public double ThinLimit;
 
     public override void NodeGUI()
     {
@@ -48,6 +53,7 @@ public class NodeGridMorphGroupFilter : NodeBase
         KnobValueField(ThresholdKnob, ref Threshold);
         KnobValueField(MinGroupSizeKnob, ref MinGroupSize);
         KnobValueField(MaxGroupSizeKnob, ref MaxGroupSize);
+        KnobValueField(ThinLimitKnob, ref ThinLimit);
 
         GUILayout.EndVertical();
 
@@ -60,14 +66,17 @@ public class NodeGridMorphGroupFilter : NodeBase
         var threshold = GetIfConnected<double>(ThresholdKnob);
         var minGroupSize = GetIfConnected<double>(MinGroupSizeKnob);
         var maxGroupSize = GetIfConnected<double>(MaxGroupSizeKnob);
+        var thinLimit = GetIfConnected<double>(ThinLimitKnob);
 
         threshold?.ResetState();
         minGroupSize?.ResetState();
         maxGroupSize?.ResetState();
+        thinLimit?.ResetState();
 
         if (threshold != null) Threshold = threshold.Get();
         if (minGroupSize != null) MinGroupSize = minGroupSize.Get();
         if (maxGroupSize != null) MaxGroupSize = maxGroupSize.Get();
+        if (thinLimit != null) ThinLimit = thinLimit.Get();
     }
 
     public override bool Calculate()
@@ -79,6 +88,7 @@ public class NodeGridMorphGroupFilter : NodeBase
             SupplierOrFallback(ThresholdKnob, Threshold),
             SupplierOrFallback(MinGroupSizeKnob, MinGroupSize),
             SupplierOrFallback(MaxGroupSizeKnob, MaxGroupSize),
+            SupplierOrFallback(ThinLimitKnob, ThinLimit),
             0d, TerrainCanvas.GridFullSize
         );
 
@@ -95,6 +105,7 @@ public class NodeGridMorphGroupFilter : NodeBase
         private readonly ISupplier<double> _threshold;
         private readonly ISupplier<double> _minGroupSize;
         private readonly ISupplier<double> _maxGroupSize;
+        private readonly ISupplier<double> _thinLimit;
         private readonly double _fallback;
         private readonly int _gridSize;
 
@@ -103,12 +114,14 @@ public class NodeGridMorphGroupFilter : NodeBase
             ISupplier<double> threshold,
             ISupplier<double> minGroupSize,
             ISupplier<double> maxGroupSize,
+            ISupplier<double> thinLimit,
             double fallback, int gridSize)
         {
             _input = input;
             _threshold = threshold;
             _minGroupSize = minGroupSize;
             _maxGroupSize = maxGroupSize;
+            _thinLimit = thinLimit;
             _fallback = fallback;
             _gridSize = gridSize;
         }
@@ -119,10 +132,20 @@ public class NodeGridMorphGroupFilter : NodeBase
             var threshold = _threshold.Get();
             var minGroupSize = _minGroupSize.Get();
             var maxGroupSize = _maxGroupSize.Get();
+            var thinLimit = _thinLimit.Get();
 
-            Vector2i[] offsets = [Vector2i.AxisX, Vector2i.AxisZ, -Vector2i.AxisX, -Vector2i.AxisZ];
+            Vector2i[] offsets = [new(1, 0), new(-1, 0), new(0, 1), new(0, -1)];
 
             double[,] grid = new double[_gridSize, _gridSize];
+
+            for (int x = 0; x < _gridSize; x++)
+            {
+                for (int z = 0; z < _gridSize; z++)
+                {
+                    grid[x, z] = input.ValueAt(x, z);
+                }
+            }
+
             byte[,] state = new byte[_gridSize, _gridSize];
 
             // 0 = not visited
@@ -138,17 +161,20 @@ public class NodeGridMorphGroupFilter : NodeBase
                 {
                     if (state[x, z] == 0)
                     {
-                        var value = grid[x, z] = input.ValueAt(x, z);
-                        state[x, z] = (byte) (value >= threshold ? 1 : 3);
+                        state[x, z] = (byte) (grid[x, z] >= threshold ? 1 : 3);
                     }
 
                     if (state[x, z] == 1)
                     {
+                        var thinCount = 0;
+
                         group.Add(new Vector2i(x, z));
 
                         for (int i = 0; i < group.Count; i++)
                         {
                             var p = group[i];
+
+                            var adjacentCount = 0;
 
                             foreach (var offset in offsets)
                             {
@@ -158,21 +184,36 @@ public class NodeGridMorphGroupFilter : NodeBase
                                 {
                                     if (state[n.x, n.z] == 0)
                                     {
-                                        var value = grid[n.x, n.z] = input.ValueAt(n.x, n.z);
-                                        state[n.x, n.z] = (byte) (value >= threshold ? 1 : 3);
+                                        state[n.x, n.z] = (byte) (grid[n.x, n.z] >= threshold ? 1 : 3);
 
                                         if (state[n.x, n.z] == 1)
                                         {
+                                            adjacentCount++;
                                             group.Add(n);
                                         }
                                     }
+                                    else if (state[n.x, n.z] != 3)
+                                    {
+                                        adjacentCount++;
+                                    }
                                 }
                             }
+
+                            if (adjacentCount < 4) thinCount++;
                         }
 
-                        var groupValid = group.Count >= minGroupSize && (maxGroupSize <= 0 || group.Count <= maxGroupSize);
+                        var thinFactor = thinCount / (double) group.Count;
+
+                        var groupValid = (group.Count >= minGroupSize || minGroupSize <= 0) &&
+                                         (group.Count <= maxGroupSize || maxGroupSize <= 0) &&
+                                         (thinFactor <= thinLimit || thinLimit <= 0);
 
                         foreach (var pos in group) state[pos.x, pos.z] = (byte) (groupValid ? 2 : 3);
+
+                        #if DEBUG
+                        var dPos = new Vector2d(group[0].x + 0.5, group[0].z + 0.5);
+                        PathTracer.DebugLine(new TraceDebugLine(null, dPos, 1, 0, $"{thinCount} / {group.Count} = {thinFactor:F2}"));
+                        #endif
 
                         group.Clear();
                     }
@@ -196,6 +237,7 @@ public class NodeGridMorphGroupFilter : NodeBase
             _threshold.ResetState();
             _minGroupSize.ResetState();
             _maxGroupSize.ResetState();
+            _thinLimit.ResetState();
         }
     }
 }
